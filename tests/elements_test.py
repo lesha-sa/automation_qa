588a29835722eaad96523c26aeadc16c7e4e0f28
Safrayter <leshkasa@gmail.com>
2024-05-20 22:01:56 +0300

    update elements_test.py add test: -test_web_table_update_person_info -test_web_table_delete_person_info -test_web_table_change_count_row

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -17,7 +17,6 @@ class TestElements:
             assert current_address == output_cur_address, "the current address does not match"
             assert permanent_address == output_per_address, "the permanent address does not match"
 
-
     class TestCheckBox:
 
         def test_check_box(self, driver):
@@ -28,7 +27,6 @@ class TestElements:
             input_checkbox = check_box_page.get_checked_checkboxes()
             output_result = check_box_page.get_output_result()
             assert input_checkbox == output_result, 'checkboxes have not been selected'
-
 
     class TestRadioButton:
 
@@ -45,7 +43,6 @@ class TestElements:
             assert output_impressive == 'Impressive', "'Impressove' have not been selected"
             assert output_no == "No", "'No' have not been selected"
 
-
     class TestWebTable:
 
         def test_web_table_add_person(self, driver):
@@ -55,7 +52,6 @@ class TestElements:
             table_result = web_table_page.check_new_added_person()
             assert new_person in table_result
 
-
         def test_web_table_search_person(self, driver):
             web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
             web_table_page.open()
@@ -63,3 +59,29 @@ class TestElements:
             web_table_page.search_some_people(key_word)
             table_result = web_table_page.check_search_person()
             assert key_word in table_result, "the person wa not found in the table"
+
+        def test_web_table_update_person_info(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            lastname = web_table_page.add_new_person()[1]
+            web_table_page.search_some_people(lastname)
+            age = web_table_page.update_person_info()
+            row = web_table_page.check_search_person()
+            time.sleep(5)
+            assert age in row, "the person card has not been changed"
+
+        def test_web_table_delete_person_info(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            email = web_table_page.add_new_person()[3]
+            web_table_page.search_some_people(email)
+            web_table_page.delete_person()
+            text = web_table_page.check_deleted()
+            assert text == "No rows found"
+
+        def test_web_table_change_count_row(self, driver):
+            web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+            web_table_page.open()
+            count = web_table_page.select_up_to_some_rows()
+            assert count == [5, 10, 20, 25, 50,
+                             100], "The number of rows in the table has not been changed or has changed icorrectly"
